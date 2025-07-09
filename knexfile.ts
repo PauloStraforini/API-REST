f6f1c81aada14469557275c3a20d07ccdcffbe92
PauloStraforini <paulostraforini@gmail.com>
2025-07-09 19:59:36 -0300

    refactor: restructure knexfile to define config directly and improve migration path resolution

--- knexfile.ts
+++ knexfile.ts
@@ -1,3 +1,28 @@
-import { config } from './src/database'
+import path from 'path'
+import { Knex } from 'knex'
 
-export default config
+export const config: { [key: string]: Knex.Config } = {
+  development: {
+    client: 'sqlite3', // ou 'pg', 'mysql', etc.
+    connection: {
+      filename: './src/db/database.sqlite',
+    },
+    migrations: {
+      directory: path.resolve(__dirname, '../db/migrations'), // 👈 caminho absoluto resolvido corretamente
+      extension: 'ts', // ou 'js' se for JavaScript
+    },
+    useNullAsDefault: true,
+  },
+
+  production: {
+    client: 'sqlite3',
+    connection: {
+      filename: './src/db/database.sqlite',
+    },
+    migrations: {
+      directory: path.resolve(__dirname, '../db/migrations'), // 👈 mesmo caminho
+      extension: 'ts',
+    },
+    useNullAsDefault: true,
+  },
+}
